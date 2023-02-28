8e07777596bed6daed652eca7751b5e5cbb6b3d6
Dennis Dinh <hungdnv@vmodev.com>
2023-02-28 11:06:03 +0700

    test commit (please ignore)

---
+++ test.py
@@ -0,0 +1,27 @@
+student_num = input('Input number of student: ')
+student_id = input('Input student ID: ')
+student_name = input('Input student name: ')
+student_dob = input('Input student Date of Birth: ')
+course_num = input('Input number of courses: ')
+course_id = input('Input course ID: ')
+course_name = input('Input course name: ')
+
+information = {
+    'student_num': 0,
+    'student_list': [],
+    'course_num': 0,
+    'course_list': []
+}
+
+student_info = {
+    'student_id': '',
+    'student_name': '',
+    'student_dob': ''
+}
+
+course_info = {
+    'course_id': '',
+    'course_name': ''
+}
+
+information.student_list.append
